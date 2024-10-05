53d2217728f15df2d78a5489f0f4deb41a2d7c1e
timussa <timussa@yahoo.com>
2024-10-05 23:08:48 +0300

    The first function is solved

--- extra-task-1.cpp
+++ extra-task-1.cpp
@@ -1,6 +1,13 @@
+#include <iostream>
+#include <assert.h>
+#include <cmath>
+#include <cfloat>
+using namespace std;
+
 double seconds_difference(double time_1, double time_2)
 {
-    // your implementation goes here...
+    
+    return time_2 - time_1;
     
     /*    
         Return the number of seconds later that a time in seconds
@@ -22,6 +29,7 @@ double seconds_difference(double time_1, double time_2)
 
 double hours_difference(double time_1, double time_2)
 {
+    return 0;
     /*
         Return the number of hours later that a time in seconds
         time_2 is than a time in seconds time_1.
@@ -38,10 +46,12 @@ double hours_difference(double time_1, double time_2)
         >>> hours_difference(1800.0, 1800.0)
         0.0
     */
+    
 }
 
 double to_float_hours(int hours, int minutes, int seconds)
 {
+    return 0;
     /*
         Return the total number of hours in the specified number
         of hours, minutes, and seconds.
@@ -61,6 +71,7 @@ double to_float_hours(int hours, int minutes, int seconds)
 
 double to_24_hour_clock(double hours)
 {
+    return 0;
     /*
         hours is a number of hours since midnight. Return the
         hour as seen on a 24-hour clock.
@@ -111,6 +122,7 @@ double to_24_hour_clock(double hours)
 
 double time_to_utc(int utc_offset, double time)
 {
+    return 0;
     /*
         Return time at UTC+0, where utc_offset is the number of hours away from
         UTC+0.
@@ -139,6 +151,7 @@ double time_to_utc(int utc_offset, double time)
 
 double time_from_utc(int utc_offset, double time)
 {
+    return 0;
     /*
         Return UTC time in time zone utc_offset.
 
@@ -166,4 +179,18 @@ double time_from_utc(int utc_offset, double time)
         >>> time_from_utc(+1, 23.0)
         0.0
     */
+}
+
+void main()
+{
+    setlocale(LC_ALL, "");
+
+    //Tests for seconds_difference()
+    assert(fabs(seconds_difference(1800.0, 3600.0) - 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(3600.0, 1800.0) + 1800.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 2160.0) - 360.0) < DBL_EPSILON);
+    assert(fabs(seconds_difference(1800.0, 1800.0)) < DBL_EPSILON);
+
+
+    system("pause");
 }
